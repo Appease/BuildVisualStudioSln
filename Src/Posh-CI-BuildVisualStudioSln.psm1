f12af57276c571487faa81da2dd26283ff01235e
Chris Dostert <chris.dostert@live.com>
2015-02-14 00:35:52 -0800

    1) added default of building all .sln's within project root 2) reorganized source

--- Src/Posh-CI-BuildVisualStudioSln.psm1
+++ Src/Posh-CI-BuildVisualStudioSln.psm1
@@ -2,9 +2,15 @@
 $ErrorActionPreference = "Stop"
 
 function Invoke-CIStep(
-[string[]][Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]$SlnFilePaths,
+[string][Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]$PoshCIProjectRootDirPath,
+[string[]][Parameter(ValueFromPipelineByPropertyName=$true)]$SlnFilePaths,
 [string][Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]$PathToMsBuildExe){
     
+    # default to recursively picking up any .sln files below the project root directory path
+    if(!$SlnFilePaths){
+        $SlnFilePaths = Get-ChildItem -file -Recurse -Path "$PoshCIProjectRootDirPath" -Include '*.sln'
+    }
+
     foreach($slnFilePath in $SlnFilePaths)
     {        
         # invoke msbuild
